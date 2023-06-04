ee0041bf083a4310b9b70233c6eb398d222b5975
EC2 Default User <ec2-user@ip-172-31-49-43.ec2.internal>
2023-06-04 15:41:22 +0000

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,85 @@
+terraform {
+required_providers {
+  aws = {
+  source = "hashicorp/aws"
+  version = ">= 3.27"
+ }
+}
+
+  required_version = ">=0.14"
+} 
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+
+# Data source for AMI id
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Create an EC2 instance
+resource "aws_instance" "ec2_instance" {
+  ami           = data.aws_ami.latest_amazon_linux.id  
+  instance_type = "t2.micro"  
+  subnet_id     = "subnet-0398991f858a33c9e"  
+  security_groups        = [aws_security_group.assignment1.id]
+}
+
+#security Group
+resource "aws_security_group" "assignment1" {
+  name        = "assignment"
+  description = "Allow  inbound traffic"
+  vpc_id      = "vpc-0fdd5ee4ff648355c" 
+
+  ingress {
+    description      = "HTTP from everywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH from everywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description      = "HTTPS from everywhere"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+   
+  ingress {
+    description      = "MYSQL/Aurora from everywhere"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+ }
+  resource "aws_ecr_repository" "webapp_repository" {
+  name = "webapp1"
+}
+
+resource "aws_ecr_repository" "mysql_repository" {
+  name = "mysql1"
+}
